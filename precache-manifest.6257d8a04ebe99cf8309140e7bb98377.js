77ddf4eebf597d92fb1a24934c77202ee10e9128
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-19 20:19:02 +0000

    Deploy Vincent-hide/week05_comp3104_lab_exec to github.com/Vincent-hide/week05_comp3104_lab_exec.git:gh-pages

---
+++ precache-manifest.6257d8a04ebe99cf8309140e7bb98377.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "55532f194b951b5a4bc2",
+    "url": "/static/css/main.ed106e13.chunk.css"
+  },
+  {
+    "revision": "55532f194b951b5a4bc2",
+    "url": "/static/js/main.55532f19.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "c74128e61607626935020a76327c7d9b",
+    "url": "/index.html"
+  }
+];
